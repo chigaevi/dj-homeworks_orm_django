9e74cd469c835b6c76878449573817ad2c59e9e5
Chigaev ILia <104361490+chigaevi@users.noreply.github.com>
2023-03-05 18:32:55 +0700

    commit 3

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -21,17 +21,29 @@ def books_view_by_pub_date(request, pub_date):
     template = 'books/filtred_books_list.html'
     all_books = Book.objects.all()
     fitred_books = Book.objects.filter(pub_date=pub_date)
-    # pub_dates_list = []
-    # for book in all_books:
-    #     pub_dates_list.append(str(book['pub_date']))
-    # pub_dates_list = list(set(pub_dates_list))
-    # print(pub_dates_list)
+    pub_dates_list = []
+    for book in all_books:
+        pub_dates_list.append(str(book.pub_date))
+    pub_dates_list = list(set(pub_dates_list))
+    print(pub_dates_list)
+    num_date = pub_dates_list.index(pub_date)
+    print(num_date)
+    if num_date == 0:
+        next_date = pub_dates_list[1]
+        prev_date = None
+    elif  num_date == len(pub_dates_list) - 1:
+        prev_date = pub_dates_list[num_date - 1]
+        next_date = None
+    else:
+        next_date = pub_dates_list[num_date + 1]
+        prev_date = pub_dates_list[num_date - 1]
 
 
 
     context = {
         'books': fitred_books,
         'pub_date': pub_date,
-
+        'prev_date': prev_date,
+        'next_date': next_date,
     }
     return render(request, template, context)
